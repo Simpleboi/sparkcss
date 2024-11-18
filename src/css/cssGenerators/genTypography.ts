bf4160d2b0eea3fb4bf27446e945c729292dd5ab
Biggie Cheese <Jv3studios@gmail.com>
2024-11-18 14:46:07 -0600

    Building CSS utility generators rn

--- src/css/cssGenerators/genTypography.ts
+++ src/css/cssGenerators/genTypography.ts
@@ -1,26 +1,25 @@
-import { readFileSync} from "fs"; 
 
-export function generateTypography(): string {
-    // Read the configuration file
-    const config = JSON.parse(readFileSync('../../../config/spark.config.json', 'utf-8'));
+export function generateTypography(config: any): string {
+  // Extract the typography part of the configuration
+  const type = config.typography;
+  let cssOutput = "";
 
-    // Extract the typography part of the configuration
-    const type = config.typography;
-    let cssOutput = "";
-
-    // Generate the fontSizes utilities
-    if (type && type.fontSizes) {
-        for (const [key, size] of Object.entries(type.fontSizes)) {
-            cssOutput += `.txt-${key} { font-size: ${size}}; \n`
-        }
+  // Generate the fontSizes utilities
+  if (type && type.fontSizes) {
+    for (const [key, size] of Object.entries(type.fontSizes)) {
+      cssOutput += `.txt-${key} { font-size: ${size};}; \n`;
     }
+    cssOutput += "\n";
+  }
 
-    // Generate the fontWeight utilities
-    if (type && type.fontWeight) {
-        for (const [key, weight] of Object.entries(type.fontWeight)) {
-            cssOutput += `.weight-${key} { font-size: ${weight}}; \n`
-        }
+  // Generate the fontWeight utilities
+  if (type && type.fontWeight) {
+    for (const [key, weight] of Object.entries(type.fontWeight)) {
+      cssOutput += `.weight-${key} { font-size: ${weight};}; \n`;
     }
+    cssOutput += "\n";
+  }
 
-    return cssOutput;
+  return cssOutput;
 }
+
--- src/css/genUtils.ts
+++ src/css/genUtils.ts
@@ -1,36 +1,32 @@
-import path from "path";
 import { readFileSync, writeFileSync } from "fs";
 import { generateTypography } from "./cssGenerators/genTypography";
 
 function generateUtilities() {
   try {
-    console.log("\n Current working directory:", process.cwd());
-    console.log("\n");
+    // Read the contents of the config file
+    const config = JSON.parse(
+      readFileSync("../config/spark.config.json", "utf-8")
+    );
 
-
-    const config = JSON.parse(readFileSync('./config/spark.config.json', "utf-8"));
-
-    // Generate the CSS based on enabled utilities in the configuration
+    // Generate the CSS based on enabled utilities
     let cssOutput: string = "";
 
     if (config.utilities.typography) {
-      cssOutput += generateTypography();
+      cssOutput += generateTypography(config);
     }
-
     if (config.utilities.boxModel) {
       // cssOutput += generateBoxModel(config);
     }
-
     if (config.utilities.colors) {
       // cssOutput += generateColors(config);
     }
-
     if (config.utilities.spacing) {
       // cssOutput += generateSpacing(config);
     }
 
     // Write the generated CSS to a file
     writeFileSync(`./spark.css`, cssOutput, "utf-8");
+    console.log("CSS generated and written to spark.css")
   } catch (err) {
     console.error("Error reading configuration file:", err);
   }
